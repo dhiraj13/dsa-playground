129fded42bececb993aceb7742a9951efa32a7d4
Dhiru <dkhapangi13@outlook.com>
2024-12-05 11:59:08 +0545

    Node class

--- linkedList/firstLinkedList.js
+++ linkedList/firstLinkedList.js
@@ -10,7 +10,12 @@
 //     },
 //   },
 // };
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -21,20 +26,14 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
